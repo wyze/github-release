ce2b47770273a1fa51e7fb03e27610bb2f96536c
Neil Kistner <neil.kistner@gmail.com>
2020-03-27 23:28:14 -0500

    Generate compare URL and add to release

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,24 @@ import ghGot from 'gh-got'
 import readPkg from 'read-pkg'
 
 type UrlVersion = {
+  slug: string
   url: string
   version: string
 }
 
 type ChangesUrlVersion = UrlVersion & {
   changes: string[]
+  latest: string
 }
 
 const readFile = promisify(fs.readFile)
 
 const getUrlAndVersion = async (): Promise<UrlVersion> => {
   const { repository, version } = await readPkg()
-  const url = repository.url
-    .replace(/(^git\+|\.git$)/g, '')
-    .split('/')
-    .slice(-2)
-    .join('/')
+  const url = repository.url.replace(/(^git\+|\.git$)/g, '')
+  const slug = url.split('/').slice(-2).join('/')
 
-  return { url, version }
+  return { slug, url, version }
 }
 
 const getChanges = async () =>
@@ -36,12 +35,14 @@ const getIndex = (changes: string[], startIndex: number) =>
 
 const createGithubRelease = async ({
   changes,
+  latest,
+  slug,
   url,
   version,
 }: ChangesUrlVersion) =>
-  await ghGot(`repos/${url}/releases`, {
+  await ghGot(`repos/${slug}/releases`, {
     body: {
-      body: changes.join('\n'),
+      body: changes.join('\n') + `\n\n${url}/compare/${latest}...v${version}`,
       tag_name: `v${version}`,
     },
   })
@@ -51,13 +52,18 @@ export default async () => {
   // Push existing tags so the tag is there to create the release on
   await execa('git', ['push', '--follow-tags'])
 
-  const { url, version } = await getUrlAndVersion()
   const rawChanges = await getChanges()
   const startIndex = getIndex(rawChanges, 0) + 1
   const endIndex = getIndex(rawChanges, startIndex)
   const changes = rawChanges
     .slice(startIndex, endIndex)
     .filter((value) => value !== '')
+  const latest =
+    endIndex > 0
+      ? rawChanges[endIndex].match(/\[([^\]]+)\]/)[1]
+      : (
+          await execa('git', ['rev-list', '--max-parents=0', 'HEAD'])
+        ).stdout.slice(0, 7)
 
-  await createGithubRelease({ changes, url, version })
+  await createGithubRelease({ changes, latest, ...(await getUrlAndVersion()) })
 }
